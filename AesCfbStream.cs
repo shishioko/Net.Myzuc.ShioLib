31f2b9bef2b5f755b9f6e1bca0592fd66f6cec8c
shishioko <cuttingspoon@protonmail.com>
2024-07-06 23:30:54 +0200

    bugfix

--- AesCfbStream.cs
+++ AesCfbStream.cs
@@ -15,13 +15,13 @@ namespace Net.Myzuc.ShioLib
             {
                 await stream.WriteU8AAsync(rsa.ExportRSAPublicKey(), SizePrefix.S32V, rsa.KeySize / 8 + 32);
                 byte[] secret = RandomNumberGenerator.GetBytes(aesKeySize);
-                await stream.WriteU8AAsync(rsa.Encrypt(secret, RSAEncryptionPadding.Pkcs1), SizePrefix.S32V, aesKeySize + 11);
+                await stream.WriteU8AAsync(rsa.Encrypt(secret, RSAEncryptionPadding.Pkcs1), SizePrefix.S32V, rsa.KeySize / 8);
                 return secret;
             }
             else
             {
                 rsa.ImportRSAPublicKey(await stream.ReadU8AAsync(SizePrefix.S32V, rsa.KeySize / 8 + 32), out _);
-                return rsa.Decrypt(await stream.ReadU8AAsync(SizePrefix.S32V, aesKeySize + 11), RSAEncryptionPadding.Pkcs1);
+                return rsa.Decrypt(await stream.ReadU8AAsync(SizePrefix.S32V, rsa.KeySize / 8), RSAEncryptionPadding.Pkcs1);
             }
         }
         public static byte[] ExchangeKey(Stream stream, bool host, int aesKeySize = 32)
@@ -32,13 +32,13 @@ namespace Net.Myzuc.ShioLib
             {
                 stream.WriteU8A(rsa.ExportRSAPublicKey(), SizePrefix.S32V, rsa.KeySize / 8 + 32);
                 byte[] secret = RandomNumberGenerator.GetBytes(aesKeySize);
-                stream.WriteU8A(rsa.Encrypt(secret, RSAEncryptionPadding.Pkcs1), SizePrefix.S32V, aesKeySize + 11);
+                stream.WriteU8A(rsa.Encrypt(secret, RSAEncryptionPadding.Pkcs1), SizePrefix.S32V, rsa.KeySize / 8);
                 return secret;
             }
             else
             {
                 rsa.ImportRSAPublicKey(stream.ReadU8A(SizePrefix.S32V, rsa.KeySize / 8 + 32), out _);
-                return rsa.Decrypt(stream.ReadU8A(SizePrefix.S32V, aesKeySize + 11), RSAEncryptionPadding.Pkcs1);
+                return rsa.Decrypt(stream.ReadU8A(SizePrefix.S32V, rsa.KeySize / 8), RSAEncryptionPadding.Pkcs1);
             }
         }
         public AesCfbStream(Stream stream, byte[] secret, byte[] vector) : base(Null, Null)
@@ -53,7 +53,7 @@ namespace Net.Myzuc.ShioLib
             aes.Padding = PaddingMode.None;
             Input = stream;
             Input = new CryptoStream(stream, aes.CreateDecryptor(), CryptoStreamMode.Read);
-            Input = new CryptoStream(stream, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            Output = new CryptoStream(stream, aes.CreateEncryptor(), CryptoStreamMode.Write);
         }
     }
 }
