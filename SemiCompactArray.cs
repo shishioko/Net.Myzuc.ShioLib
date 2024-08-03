8a2bb9dd06d44300cdc816c30573c59553511215
shishioko <shishioko@proton.me>
2024-08-03 03:08:55 +0200

    fixed random shit

--- SemiCompactArray.cs
+++ SemiCompactArray.cs
@@ -1,20 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System;
 
 namespace Net.Myzuc.ShioLib
 {
-    public sealed class CompactArray : IEnumerable<int>
+    public sealed class SemiCompactArray : IEnumerable<int>
     {
         private sealed class Enumerator : IEnumerator<int>
         {
-            private readonly CompactArray Data;
+            private readonly SemiCompactArray Data;
             private int Index;
             private int Last;
             public int Current => Last;
             object IEnumerator.Current => Last;
-            public Enumerator(CompactArray data)
+            public Enumerator(SemiCompactArray data)
             {
                 Data = data;
                 Index = -1;
@@ -54,24 +53,24 @@ namespace Net.Myzuc.ShioLib
         /// </summary>
         /// <param name="bits">The bits size of each entry in the created CompactArray.</param>
         /// <param name="length">The amount of entries in the created CompactArray.</param>
-        public CompactArray(byte bits, int length)
+        public SemiCompactArray(byte bits, int length)
         {
             Contract.Requires(bits > 0);
             Contract.Requires(bits <= 32);
             Bits = bits;
             Length = length;
-            Data = new ulong[(int)MathF.Ceiling(length * bits / 64.0f)];
+            Data = new ulong[(length - 1) / (64 / bits) + 1];
         }
         /// <summary>
         /// Creates a new CompactArray from raw data.
         /// </summary>
         /// <param name="bits">Compact Level</param>
         /// <param name="data">Serialized Data</param>
-        public CompactArray(byte bits, int length, ulong[] data)
+        public SemiCompactArray(byte bits, int length, ulong[] data)
         {
             Contract.Requires(bits > 0);
             Contract.Requires(bits <= 32);
-            Contract.Requires(data.Length >= (int)MathF.Ceiling(length * bits / 64.0f));
+            Contract.Requires(data.Length >= (length - 1) / (64 / bits) + 1);
             Bits = bits;
             Length = length;
             Data = data;
@@ -102,30 +101,21 @@ namespace Net.Myzuc.ShioLib
             {
                 Contract.Requires(index >= 0);
                 Contract.Requires(index < Length);
-                int position = (int)((ulong)index * 64 / Bits);
-                ulong mask = (1UL << Bits) - 1UL;
-                int shift = (index % Bits) * Bits;
-                int data = (int)((Data[position] & (mask << shift)) >> shift);
-                if (shift + Bits <= 64) return data;
-                shift = -(shift + Bits - 64);
-                position++;
-                return (int)((Data[position] & (mask >> shift)) << shift) | data;
+                int pack = 64 / Bits;
+                return (int)((Data[index / pack] >> (index % pack * Bits)) & (1UL << Bits) - 1UL);
             }
             set
             {
                 Contract.Requires(index >= 0);
                 Contract.Requires(index < Length);
                 Contract.Requires(value < 1 << Bits);
-                ulong mask = (1UL << Bits) - 1UL;
-                int position = (int)((ulong)index * 64 / Bits);
-                int shift = (index % Bits) * Bits;
-                Data[position] &= ~(mask << shift);
-                Data[position] |= (ulong)value << shift;
-                if (shift + Bits <= 64) return;
-                shift = -(shift + Bits - 64);
-                position++;
-                Data[position] &= ~(mask >> shift);
-                Data[position] |= (ulong)value >> shift;
+                int pack = 64 / Bits;
+                int position = index / pack;
+                int shift = (index % pack) * Bits;
+                ulong data = Data[position];
+                data &= ~(((1UL << Bits) - 1UL) << shift);
+                data |= (ulong)value << shift;
+                Data[position] = data;
             }
         }
     }
